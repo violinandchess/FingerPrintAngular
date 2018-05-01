bec40bdc1b65701d1242a5b323f9142a1028bbbc
violinandchess <vibaviattigala@gmail.com>
2018-05-01 16:48:57 +0530

    Login bug

--- FrontEnd/FingerPrintWeb/src/app/app.module.ts
+++ FrontEnd/FingerPrintWeb/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

4fe6eae6b962a1551267314dfb5c27c1b64955dd
violinandchess <vibaviattigala@gmail.com>
2018-05-03 13:42:41 +0530

    login empty validation

--- FrontEnd/FingerPrintWeb/src/app/app.module.ts
+++ FrontEnd/FingerPrintWeb/src/app/app.module.ts
@@ -6,7 +6,13 @@ import { AppComponent } from './app.component';
 import {LoginComponent} from './login/login.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {  ReactiveFormsModule } from '@angular/forms';
-
+import { AlertComponent } from './_directives/alert.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { AlertService } from './_services/alert.service';
+import {UserService } from './_services/user.service';
+import {AuthenticationService} from './_services/authentification.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,10 +23,20 @@ import {  ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
+    
     
   ],
-  providers: [],
+  providers: [  AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass: JwtInterceptor,
+        multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- FrontEnd/FingerPrintWeb/src/app/login/login.component.ts
+++ FrontEnd/FingerPrintWeb/src/app/login/login.component.ts
@@ -1,31 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {Router,ActivatedRoute} from "@angular/router";
 import { NgModule } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AlertService } from '../_services/alert.service';
+import { AuthenticationService } from '../_services/authentification.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  model: any = {};
+  loading = false;
+  returnUrl: string;
 
-  loginForm: FormGroup;
+  constructor( private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService
+  
+  
+  ) 
+  {
 
-  constructor(private router:Router) {
-
-    
-
-   }
+  }
 
   ngOnInit() {
-    this.createForm();
-  }
-  private createForm() {
-    this.loginForm = new FormGroup({
-      // tslint:disable-next-line
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
-    });
-}
+   // reset login status
+        this.authenticationService.logout();
 
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+  login() {
+    console.log("hii");
+    this.loading = true;
+    this.authenticationService.login(this.model.username, this.model.password)
+        .subscribe(
+            data => {
+                this.router.navigate([this.returnUrl]);
+            },
+            error => {
+                
+                
+                        this.alertService.error(error);
+                this.loading = false;
+            });
+  }
 }

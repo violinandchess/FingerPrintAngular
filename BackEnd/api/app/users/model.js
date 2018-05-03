419588de5e3f0da6778ad1d3ba25edba38ad688c
violinandchess <vibaviattigala@gmail.com>
2018-05-03 12:42:58 +0530

    added servie arch

---
+++ BackEnd/api/app/users/model.js
@@ -0,0 +1,24 @@
+const mongoose = require( "mongoose" );
+const md5 = require( "md5" );
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema( {
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true }
+   
+}, {
+    timestamps: true,
+} );
+
+userSchema.methods.setPass = function( password ) {
+    this.password = md5( password );
+};
+
+userSchema.methods.checkPass = function( password ) {
+    return this.password === md5( password );
+};
+
+module.exports = mongoose.model( "User", userSchema );
--- BackEnd/src/config/environments/development.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: "127.0.0.1",
-    port: 1234, // change with development port
-    mongoUrl: "mongodb://localhost:27017/projectDbName", // replace "projectDbName" with a proper db name
-    logLevel: "debug", // can be chenged to error, warning, info, verbose or silly
-    secret: "superSuperSecret",
-};

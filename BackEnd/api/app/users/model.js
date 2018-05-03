ef4c88694031b8ed016df5fa6f7b29cea00cb24b
violinandchess <vibaviattigala@gmail.com>
2018-05-03 15:48:28 +0530

    added usertype in api

--- BackEnd/api/app/users/model.js
+++ BackEnd/api/app/users/model.js
@@ -7,6 +7,7 @@ const userSchema = new Schema( {
     _id: { type: String, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
+    userType:{type: String, required: true},
     name: { type: String, required: true }
    
 }, {
